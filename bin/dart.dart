99cc1b6e388b4844853bd4bf41168d949e8634b4
mdnoman58 <mdnoman51696@gmail.com>
2024-02-17 00:24:27 +0600

    hello

---
+++ bin/dart.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  String age='12';
+  String index='12123';
+  String result;
+  String name='this is noman';
+  result=age+index;//this is how we get the length of string
+  print(age.length);
+  print(index.length);//this is how we get the any value of inside the string
+  print(age[1]);
+  print(result);
+  print(name.split(''));
+  print(name.replaceAll(name,'abc'));
+
+}
+
+
+
+
